4bbfaa4b1c53325683b15d049675d0d33be4e696
Dale Seo <dale.seo@gmail.com>
2016-07-29 09:17:30 +0900

    add jade dependency

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ var express = require('express')
 var app = express();
 
 app.use(express.static('public'));
+app.set('views', './views')
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
     res.send('Hello Trello!');
+});
+
+app.get('/jade', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.listen(3000, function () {
